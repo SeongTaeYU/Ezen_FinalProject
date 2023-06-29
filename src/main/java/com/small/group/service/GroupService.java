3f40f7fe9d09e741220b4926bd9903f686093328
leeminhyuk93 <dlalsgur1003@naver.com>
2023-06-29 18:01:39 +0900

    update final

--- src/main/java/com/small/group/service/GroupService.java
+++ src/main/java/com/small/group/service/GroupService.java
@@ -1,5 +1,16 @@
 package com.small.group.service;
 
+import java.util.List;
+
+import com.small.group.dto.GroupDTO;
+import com.small.group.entity.Group;
+
+
 public interface GroupService {
 
+	Group insertGroup(GroupDTO groupData);
+	GroupDTO readGroup(Integer groupNo);
+	Group updateGroup(GroupDTO groupData);
+    Boolean deleteGroup(Integer groupNo);
+    List<GroupDTO> getgroupList();
 }
--- src/main/java/com/small/group/service/GroupServiceImpl.java
+++ src/main/java/com/small/group/service/GroupServiceImpl.java
@@ -1,7 +1,15 @@
 package com.small.group.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.small.group.dto.GroupDTO;
+import com.small.group.entity.Group;
+import com.small.group.entity.GroupCategory;
+import com.small.group.entity.Region;
+import com.small.group.entity.User;
 import com.small.group.repository.*;
 
 import lombok.RequiredArgsConstructor;
@@ -11,4 +19,108 @@ import lombok.RequiredArgsConstructor;
 public class GroupServiceImpl implements GroupService {
 
 	private final GroupRepository groupRepository;
+	private final GroupCategoryRepository groupCategoryRepository;
+	private final RegionRepository regionRepository;
+	private final UserRepository userRepository;
+	
+	/**
+	 *  DTO TO ENTITY
+	 */
+	private Group dtoToEntity(GroupDTO dto) {
+		Optional<GroupCategory> optGroupCategory = groupCategoryRepository.findById(dto.getGroupCategoryNo());
+		Optional<Region> optRegion = regionRepository.findById(dto.getRegionNo());
+		Optional<User> optUser = userRepository.findById(dto.getUserNo());
+		
+		GroupCategory groupCategory = (optGroupCategory.isPresent()) ? optGroupCategory.get() : null;
+		Region region = (optRegion.isPresent()) ? optRegion.get() : null;
+		User user = (optUser.isPresent()) ? optUser.get() : null;
+		
+		Group entity = Group.builder()
+				.groupName(dto.getGroupName())
+				.groupDescription(dto.getGroupDescription())
+				.groupCategory(groupCategory)
+				.region(region)
+				.user(user)
+				.build();
+		
+		return entity;
+	}
+	/**
+	 *  ENTITY TO DTO
+	 */
+	private GroupDTO entityToDto(Group entity) {
+		Integer groupCategoryNo = entity.getGroupCategory().getGroupCategoryNo();
+		String groupCategoryName = entity.getGroupCategory().getGroupCategoryName();
+		Integer regionNo = entity.getRegion().getRegionNo();
+		String regionName = entity.getRegion().getRegionName();
+		Integer userNo = entity.getUser().getUserNo();
+		String readerUserName = entity.getUser().getName();
+		
+		GroupDTO dto = GroupDTO.builder()
+				.groupName(entity.getGroupName())
+				.groupDescription(entity.getGroupDescription())
+				.groupCategoryNo(groupCategoryNo)
+				.groupCategoryName(groupCategoryName)
+				.regionNo(regionNo)
+				.regionName(regionName)
+				.userNo(userNo)
+				.readerUserName(readerUserName)
+				.regDate(entity.getRegDate())
+				.modDate(entity.getModDate())
+				.build();
+		return dto;
+	}
+	
+	/**
+	 * ----------------------------------
+	 * 			C / R / U / D
+	 * ----------------------------------
+	 */
+	
+	/**
+	 *	모임 저장하는 함수
+	 */
+	@Override
+	public Group insertGroup(GroupDTO groupData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 *	모임 한 개 가져오는 함수
+	 */
+	@Override
+	public GroupDTO readGroup(Integer groupNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 *	모임 수정하는 함수
+	 */
+	@Override
+	public Group updateGroup(GroupDTO groupData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 *	모임 삭제하는 함수
+	 */
+	@Override
+	public Boolean deleteGroup(Integer groupNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 *	모임 리스트를 가져오는 함수
+	 */
+	@Override
+	public List<GroupDTO> getgroupList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 }
